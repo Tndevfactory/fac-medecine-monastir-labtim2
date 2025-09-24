d1e708e5ad69a7740df10cab1b322fdbd68b6adc
tndev <tndevfactory@gmail.com>
2025-09-24 12:19:03 +0100

    change next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,24 +8,42 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
+        protocol: "https",
+        hostname: "images.pexels.com",
       },
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
+        protocol: "https",
+        hostname: "placehold.co",
       },
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5000',
-        pathname: '/uploads/**', // Explicitly limit to uploads
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000",
+        pathname: "/uploads/**", // Explicitly limit to uploads
       },
       {
-        protocol: 'http',
-        hostname: '172.29.112.1', // Backend IP
-        port: '5000',
-        pathname: '/uploads/**', // Explicitly limit to uploads
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000",
+        pathname: "/uploads/**", // Explicitly limit to uploads
+      },
+      {
+        protocol: "http",
+        hostname: "lab-tim.org",
+      },
+      {
+        protocol: "http",
+        hostname: "lab-tim.org",
+      },
+      {
+        protocol: "http",
+        hostname: "102.211.209.201",
+      },
+      {
+        protocol: "http",
+        hostname: "172.29.112.1", // Backend IP
+        port: "5000",
+        pathname: "/uploads/**", // Explicitly limit to uploads
       },
     ],
   },
@@ -34,20 +52,20 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/uploads/:path*',
-        destination: 'http://localhost:5000/uploads/:path*',
+        source: "/uploads/:path*",
+        destination: "http://localhost:5000/uploads/:path*",
       },
       {
-        source: '/uploads/:path*',
-        destination: 'http://172.29.112.1:5000/uploads/:path*',
+        source: "/uploads/:path*",
+        destination: "http://172.29.112.1:5000/uploads/:path*",
       },
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        source: "/api/:path*",
+        destination: "http://localhost:5000/api/:path*",
       },
-       {
-        source: '/api/:path*',
-        destination: 'http://172.29.112.1:5000/uploads/:path*',
+      {
+        source: "/api/:path*",
+        destination: "http://172.29.112.1:5000/uploads/:path*",
       },
     ];
   },
